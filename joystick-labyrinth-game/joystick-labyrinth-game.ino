40545c4b0f1fd9aecc389370088e7cab61e8289d
Lao, Seong Hok <slao8@gatech.edu>
2024-04-24 17:52:07 -0400

    adds gameWin logic

--- joystick-labyrinth-game/joystick-labyrinth-game.ino
+++ joystick-labyrinth-game/joystick-labyrinth-game.ino
@@ -5,7 +5,7 @@
 
 #define VRX_PIN  A0 // Arduino pin connected to VRX pin
 #define VRY_PIN  A1 // Arduino pin connected to VRY pin
-#define JOYSTICK_BUTTON_PIN 6 // Arduino pin connected to button pushed pin
+int JOYSTICK_BUTTON_PIN = 2; // Arduino pin connected to button pushed pin
 
 const int rs = 52, en = 50, d4 = 53, d5 = 51, d6 = 49, d7 = 47; // pin nums for lcd
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7); // created lcd object
@@ -27,6 +27,15 @@ unsigned long currentTime = 0;
 unsigned long offset = 0;
 
 int ct = 0; // counter for how many cycles the button is held for
+
+int melody[] = {
+  E5, E5, E5, C5, E5, E5, G5, G5, C5, C5
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 8, 8, 12, 8, 8, 4, 4
+};
 
 void setup() {
   s0.attach(8);
@@ -54,7 +63,7 @@ void loop() {
   // Serial.print("xVal: ");
   // Serial.println(xVal);
   // Serial.print("yVal: ");
-  // // Serial.println(yVal);
+  // Serial.println(yVal);
   // delay(100);
 
   if (xVal > 482 && xVal < 542) {
@@ -76,8 +85,8 @@ void loop() {
   currentTime = millis() - offset;
   lcd.print(currentTime / 1000);
 
-  //Serial.println(digitalRead(JOYSTICK_BUTTON_PIN));
-  // delay(100);
+  Serial.println(digitalRead(2));
+  delay(100);
   if (digitalRead(JOYSTICK_BUTTON_PIN) == 0) {
     ct += 1;
   } else {
@@ -88,19 +97,32 @@ void loop() {
   qtr.read(sensorValues);
   // Serial.println(sensorValues[0]);
   // delay(100);
-
   // print the sensor values as numbers from 0 to 1023, where 0 means maximum
   // reflectance and 1023 means minimum reflectance
-  // if (sensorValues[0] < 200) {
-  //   lcd.clear();
-  //   lcd.setCursor(0, 0);
-  //   lcd.print("YOU WON!");
-  //   tone(3, C1, 1000);
-  //   delay(3000);
-  // } else {
-  //   lcd.setCursor(0, 1);
-  //   lcd.print("In PROgress");
-  // }
+  if (sensorValues[0] < 200) {
+    gameWin();
+  } else {
+    lcd.setCursor(0, 1);
+    if (currentTime < 10) {
+      lcd.print("MOVE THE BALL!");
+    }
+    if (currentTime >= 10) {
+      lcd.print("HURRY UP!");
+    }
+  }
+}
+
+void gameWin() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("YOU WON!");
+  for (int thisNote = 0; thisNote < 10; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(8);
+  }
 }
 
 void resetTimer() {
